f451a59696bc49babbd703dc2da4d25ab79b484b
deven33 <devendraameta33@gmail.com>
2021-02-10 18:49:37 +0530

    streams-> allMatch, noneMatch, anyMAtch

--- functional-programming-with-java/src/programming/FP03FunctInterfaces.java
+++ functional-programming-with-java/src/programming/FP03FunctInterfaces.java
@@ -2,6 +2,9 @@ package programming;
 
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
@@ -19,12 +22,30 @@ public class FP03FunctInterfaces {
 			Random random = new Random();
 			return random.nextInt(1000);
 		};
-
 		System.out.println(supplier.get());
 
 		// Unary
 		UnaryOperator<Integer> unary = (x) -> 2 * x;
 		System.out.println(unary.apply(4));
+
+		// Bipredicate
+		BiPredicate<Integer, String> bipredicate = (num, str) -> {
+			return num > 10 && str.length() > 10;
+		};
+		System.out.println(bipredicate.test(12, "deven"));
+
+		// BiFunction
+		BiFunction<Integer, String, String> biFun = (num, str) -> {
+			return "dev" + " " + str;
+		};
+		System.out.println(biFun.apply(12, "ameta"));
+
+		// Biconsumer
+		BiConsumer<String, String> biCon = (s1, s2) -> {
+			System.out.println(s1);
+			System.out.println(s2);
+		};
+		biCon.accept("dev", "amet");
 	}
 
 	protected static void filterAndPrint(List<Integer> numbers, Predicate<? super Integer> predicate) {
---
+++ functional-programming-with-java/src/programming/FP05CustomClass.java
@@ -0,0 +1,81 @@
+package programming;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+class Course {
+	private String name;
+	private int noOfStudent;
+	private int reviewScore;
+	private String course;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNoOfStudent() {
+		return noOfStudent;
+	}
+
+	public void setNoOfStudent(int noOfStudent) {
+		this.noOfStudent = noOfStudent;
+	}
+
+	public int getReviewScore() {
+		return reviewScore;
+	}
+
+	public void setReviewScore(int reviewScore) {
+		this.reviewScore = reviewScore;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public Course(String name, int noOfStudent, int reviewScore, String course) {
+		super();
+		this.name = name;
+		this.noOfStudent = noOfStudent;
+		this.reviewScore = reviewScore;
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [name=" + name + ", noOfStudent=" + noOfStudent + ", reviewScore=" + reviewScore + "]";
+	}
+
+}
+
+public class FP05CustomClass {
+
+	public static void main(String[] args) {
+		List<Course> course = List.of( 	new Course("Java", 100, 34, "Basic"), 
+										new Course("Python", 101, 54, "Basic"),
+										new Course("React", 101, 314, "Basic"), 
+										new Course("Spring", 109, 94, "Basic"),
+										new Course("Hibernate", 144, 14, "Basic")
+									);
+
+		// All match, none match, any match
+
+		// All match with reviews > 90
+		Predicate<Course> matchRulegreaterthan95 = courses -> courses.getReviewScore() > 95;
+		Predicate<Course> matchRulegreaterthan10 = courses -> courses.getReviewScore() > 10;
+		
+		System.out.println(course.stream().allMatch(matchRulegreaterthan95));
+		System.out.println(course.stream().noneMatch(matchRulegreaterthan10));
+		System.out.println(course.stream().anyMatch(matchRulegreaterthan95));
+
+	}
+
+}
